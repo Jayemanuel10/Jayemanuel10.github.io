d1c2c9419b5c73172114cbddf2e1a858e20f8782
Jayemanuel10 <jaymanuel@techpro360solutions.com>
2026-06-06 14:46:32 +0800

    Delete hms-frontend-main directory

--- hms-frontend-main/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/react-swc'
-
-export default defineConfig({
-  plugins: [react()],
-  base: 'portfolio.github.io', // Must match your GitHub repo name exactly
-})
